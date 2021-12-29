c5f45a2b55935f8bbc136092f17a72189afb3363
Faizan <faizan10933@gmail.com>
2021-12-29 22:35:48 +0530

    cruddone

---
+++ base/forms.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.forms import ModelForm, fields
+from .models import Room
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
