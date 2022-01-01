069fdc49ff1dbbd5334435d54e896d8d561b814e
Faizan <faizan10933@gmail.com>
2022-01-01 23:02:06 +0530

    login logout

--- base/views.py
+++ base/views.py
@@ -3,10 +3,13 @@ from django import forms
 from django.forms.forms import Form
 from django.http.request import QueryDict
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.urls.conf import include
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # from django.http import HttpResponse
 # Create your views here.
 
@@ -15,6 +18,31 @@ from django.db.models import Q
 #     {'id':2, 'name':'design with me '},
 #     {'id':3, 'name':'frontend developers'}
 # ]
+
+def loginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        try:
+            user= User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exists')
+
+        user=authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username or password not exists')
+
+    context={}
+    return render(request,'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q')!=None else ''
